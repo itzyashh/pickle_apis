7be09edf070133bf9a483e6fae2b783c300c72af
Yash Jadhav <yashjadhav1411@rocketmail.com>
2023-11-21 22:55:38 +0530

    Update post.controller.js

--- src/controllers/postController/post.controller.js
+++ src/controllers/postController/post.controller.js
@@ -2,6 +2,7 @@
 import PostModel from '../../models/post.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 const baseUrl = 'http://localhost:3000/';
 
 const createPost = async (req, res) => {
@@ -26,12 +27,72 @@ const createPost = async (req, res) => {
 }
 
 const getPosts = async (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const user_id = req.query.user_id;
+    const totalPosts = await PostModel.countDocuments({});
+    console.log(totalPosts);
+
+    const totalPage = Math.ceil(totalPosts / limit);
+    const startIndex = (page - 1) * limit;
+
 
     try {
-        const result = await PostModel.find({});
-        res.status(200).json({ result ,length:result.length });
+        const result = await PostModel.aggregate([
+            {
+                $lookup: {
+                    from: "likes",
+                    let: { post_id: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ["$post_id", "$$post_id"] },
+                                        { $eq: ["$user_id", new mongoose.Types.ObjectId(user_id)] },
+                                    ],
+                                },
+                            },
+                        },
+                    ],
+                    as: "likes",
+                }
+            },
+            {
+                $addFields: {
+                    isLiked: { 
+                        $cond: { 
+                            if: { $gt: [{ $size: "$likes" }, 0] }, 
+                            then: true, 
+                            else: false 
+                        
+                    }
+                }
+            }
+            },
+            {
+                $project: {
+                    likes: 0,
+                }
+            },
+            {
+                $skip: startIndex,
+            },
+            {
+                $limit: limit,
+            },
+            {
+                $sort: {
+                    createdAt: -1,
+                }
+            },
+        ]);
+        res.status(200).json({ result ,length:result.length,
+            totalPage: totalPage,
+            currentPage: page,
+        });
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
